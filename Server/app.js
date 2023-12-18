38293c6579473fbbaa6010dcac5c2c3fe5aaeb89
Alvin Pratama <palveeen22@gmail.com>
2023-12-18 16:46:51 +0700

    feat(server): server, status:done

--- Server/app.js
+++ Server/app.js
@@ -4,7 +4,30 @@ const http = require("http");
 const app = express();
 const server = http.createServer(app);
 const socket = require("socket.io");
-const io = socket(server);
+const io = socket(server)(server, {
+  cors: {
+    origin: "",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.emit("me", socket.id);
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("callEnded");
+  });
+  socket.on("callUser", (data) => {
+    io.to(data.userToCall).emit("callUser", {
+      signal: data.signalData,
+      from: data.from,
+      name: data.name,
+    });
+    socket.on("answwerCall", (data) => {
+      io.to(data.to).emit("callAccepted"), data.signal;
+    });
+  });
+});
 
 server.listen(port, () => {
   console.log(`слушаем на порт ${port}`);
